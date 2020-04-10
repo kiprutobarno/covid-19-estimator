aa7a5b9bdd7674856cd262223c8dabe1c289a0b0
kipruto <barxwells@gmail.com>
2020-04-10 12:16:45 +0300

    bug-fix(ventilators):use Math.floor()

--- src/helpers.js
+++ src/helpers.js
@@ -50,7 +50,8 @@ const getPercentage = (figure, quotient) => figure * quotient;
  * @param {number} infections
  * @returns {number}
  */
-const getProjectedSeverePositiveCases = (infections) => Math.ceil(getPercentage(infections, 0.15));
+const getProjectedSeverePositiveCases = (infections) =>
+  Math.ceil(getPercentage(infections, 0.15));
 
 /**
  * returns available hospital beds by requested time
@@ -68,14 +69,16 @@ const getAvailableHospitalBedsByRequestedTime = (capacity, cases) => {
  * @param {number} infections
  * @returns {number}
  */
-const getCasesForICUByRequestedTime = (infections) => Math.ceil(getPercentage(infections, 0.05));
+const getCasesForICUByRequestedTime = (infections) =>
+  Math.ceil(getPercentage(infections, 0.05));
 
 /**
  * computes projected estimation of cases that require ventilators
  * @param {number} infections
  * @returns {number}
  */
-const getCasesVentilatorsByTime = (infections) => Math.ceil(getPercentage(infections, 0.02));
+const getCasesVentilatorsByTime = (infections) =>
+  Math.floor(getPercentage(infections, 0.02));
 
 const getEconomicImpact = (infections, region, duration) => {
   const incomePopulation = Math.floor(
