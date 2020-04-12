a2a521b54f14da14e4907bae7326f014c57cae4f
kipruto <barxwells@gmail.com>
2020-04-13 00:52:03 +0300

    bug(code):fix status code

--- src/routes.js
+++ src/routes.js
@@ -7,13 +7,13 @@ const routes = Router();
 routes.post('/', (req, res) => {
   const payload = req.body;
   const data = covid19ImpactEstimator(payload);
-  return res.json(data);
+  return res.status(201).json(data);
 });
 
 routes.post('/json', (req, res) => {
   const payload = req.body;
   const data = covid19ImpactEstimator(payload);
-  return res.json(data);
+  return res.status(201).json(data);
 });
 
 routes.post('/xml', (req, res) => {
@@ -21,8 +21,8 @@ routes.post('/xml', (req, res) => {
   const data = covid19ImpactEstimator(payload);
   const xmlBuilder = new xml2js.Builder();
   const xmlData = xmlBuilder.buildObject(data);
-  res.set('Content-Type', 'application/xml');
-  return res.send(xmlData);
+  res.type('Content-Type', 'application/xml');
+  return res.status(201).send(xmlData);
 });
 
 export default routes;
