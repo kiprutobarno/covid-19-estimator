02b4e11e9bd9715416977edfc374a6166c254c9a
kipruto <barxwells@gmail.com>
2020-04-13 23:40:23 +0300

    chore(refactor):refactor routes

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,26 @@
 import { Router } from 'express';
 import json2xml from 'json2xml';
 import covid19ImpactEstimator from './estimator';
-import { inRequest, dataLog } from './logs';
 
 const routes = Router();
 
-const respond = (req, res) => {
-  const { date, outResponse } = req.params;
-
-  const data = covid19ImpactEstimator(req.body);
-
-  outResponse(date, req, 200);
+routes.post('/', (req, res) => {
+  const payload = req.body;
+  const data = covid19ImpactEstimator(payload);
   return res.json(data);
-};
-
-routes.route('/').post(inRequest, respond);
-
-routes.route('/json').post(inRequest, respond);
-
-routes.route('/xml').post(inRequest, (req, res) => {
-  const { date, outResponse } = req.params;
-  const data = covid19ImpactEstimator(req.body);
-  res.set('Content-Type', 'application/xml');
-  outResponse(date, req, 200);
-  return res.send(json2xml(data));
 });
 
-routes.route('/logs').get(inRequest, dataLog);
+routes.post('/json', (req, res) => {
+  const payload = req.body;
+  const data = covid19ImpactEstimator(payload);
+  return res.json(data);
+});
+
+routes.post('/xml', (req, res) => {
+  const payload = req.body;
+  const data = covid19ImpactEstimator(payload);
+  res.set('Content-Type', 'application/xml');
+  return res.send(json2xml(data));
+});
 
 export default routes;
