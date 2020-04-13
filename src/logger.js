c050ade8f8eb9f3f58fe1fea4914c11a0234208b
kipruto <barxwells@gmail.com>
2020-04-14 01:27:03 +0300

    bug(logger):fix missing directory issue - ensure log directory is created on application run

--- src/logger.js
+++ src/logger.js
@@ -6,20 +6,23 @@ const logPath = path.join(__dirname, './logs/', 'log.txt');
 const logFile = fs.createWriteStream(logPath, { flags: 'a' });
 
 if (fs.existsSync(logPath)) {
-  fs.createWriteStream(logPath, { flags: 'a' });
+  stream: logFile;
 } else {
-  fs.mkdirSync(path.join(__dirname, './logs/'));
+  fs.mkdirSync(path.dirname(logPath));
+  fs.writeFileSync(logPath, { flags: 'wx' });
+  console.log('access.log created!');
 }
 
 const logger = morgan(
-  (tokens, req, res) => [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    parseInt(tokens['response-time'](req, res).toString()) < 10
-      ? `0${parseInt(tokens['response-time'](req, res).toString())}ms`
-      : `${parseInt(tokens['response-time'](req, res).toString())}ms`
-  ].join('\t\t'),
+  (tokens, req, res) =>
+    [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      parseInt(tokens['response-time'](req, res).toString()) < 10
+        ? `0${parseInt(tokens['response-time'](req, res).toString())}ms`
+        : `${parseInt(tokens['response-time'](req, res).toString())}ms`
+    ].join('\t\t'),
   {
     stream: logFile
   }
